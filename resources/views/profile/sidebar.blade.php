0a698ed5b86229cbd2cbe31f10d01a3b1945fbe6
sapna <developerweb6@gmail.com>
2022-07-29 22:33:31 +0530

    first commit

---
+++ resources/views/profile/sidebar.blade.php
@@ -0,0 +1,23 @@
+<nav class="pcoded-navbar">
+    <div class="sidebar_toggle"><a href="#"><i class="icon-close icons"></i></a></div>
+    <div class="pcoded-inner-navbar main-menu">
+
+        <div class="pcoded-navigatio-lavel" data-i18n="nav.category.navigation">Layout</div>
+        <ul class="pcoded-item pcoded-left-item">
+            <li class="active">
+                <a href="index.html">
+                    <span class="pcoded-micon"><i class="ti-home"></i><b>D</b></span>
+                    <span class="pcoded-mtext" data-i18n="nav.dash.main">Create Video</span>
+                    <span class="pcoded-mcaret"></span>
+                </a>
+            </li>
+            <li class="pcoded-hasmenu">
+                <a href="javascript:void(0)">
+                    <span class="pcoded-micon"><i class="ti-layout-grid2-alt"></i></span>
+                    <span class="pcoded-mtext"  data-i18n="nav.basic-components.main">Your Video</span>
+                    <span class="pcoded-mcaret"></span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
