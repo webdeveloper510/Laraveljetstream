c8fc1b95b5686c126eccdac85221b4150a883a9c
sapna <developerweb6@gmail.com>
2022-08-03 04:24:39 +0530

    changes update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,13 +7,26 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                {{-- <x-jet-welcome /> --}}
+             <div class="row">
+                <div class="col-md-4">
+               {{-- @include('profile.sidebar') --}}
+                </div>
+             </div>
             </div>
         </div>
     </div>
+    @push('js')
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+    <script>
+        console.log('fsdffdssdf');
+        var user = {!! json_encode((array)auth()->user()) !!};
+       console.log('user')
+    </script>
+@endpush
 </x-app-layout>
 
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -18,7 +19,9 @@
             body {
                 font-family: 'Nunito', sans-serif;
             }
+
         </style>
+
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
@@ -55,7 +58,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
+                                    Laravel has, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
                                 </div>
                             </div>
                         </div>
