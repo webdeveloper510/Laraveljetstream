b87882b8c74a815f5974db3b8978a0376f06d010
sapna <developerweb6@gmail.com>
2022-08-03 20:52:24 +0530

    changes

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,7 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
+
 
     </head>
     <body>
