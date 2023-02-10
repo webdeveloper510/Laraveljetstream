42fcb436a13af3d0edc6d6bcddb5489d116a780b
Shreyacodenomad <shreyacodenomad@gmail.com>
2023-02-10 06:24:36 +0530

    changes

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -279,9 +279,11 @@ div#google_translate_element {
                     <x-jet-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                            <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                <img class="h-8 w-8 rounded-full object-cover" src="{{ 'https://spaces3.nyc3.digitaloceanspaces.com/' .  Auth::user()->profile_photo_path }}" alt="{{  Auth::user() ? Auth::user()->name : ''}}" />
+                            <button class="text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                <img class="h-8 w-8 rounded-full object-cover" src="{{ 'https://spaces3.nyc3.digitaloceanspaces.com/' .  Auth::user()->profile_photo_path }}" alt="" />
+                                <span>{{  Auth::user() ? Auth::user()->name : ''}}</span>
                             </button>
+
                             @else
                             <span class="inline-flex rounded-md">
                                 <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
